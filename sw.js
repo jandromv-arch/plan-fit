1a4bc8c5d0711eecacc6083efb5861987441cc83
jandromv-arch <jandromv@gmail.com>
2026-09-01 05:55:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'planfit-v21';
+const CACHE = 'planfit-v22';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192-v2.png', './icon-512-v2.png'];
 
 self.addEventListener('install', e => {
